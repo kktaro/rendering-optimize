2429edf7ec5590f7a4a290539b9e14d95337f22a
kktaro <85218317+kktaro@users.noreply.github.com>
2021-10-26 16:16:34 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1+3, [])
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={(event) => onChangeText(event)} />
+      <br />
+      <br />
+      <button onClick={() => onClickOpen()}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
